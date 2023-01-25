a6514efc67c9961edbf0aad05fd6cce5cf2648c2
tvkalex <tvkalex@yandex.ru>
2023-01-25 18:54:16 +0300

    Add program code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+ResultArrayAfterCheck(array1, array2);
+PrintArray(array2);
+
+void ResultArrayAfterCheck(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
